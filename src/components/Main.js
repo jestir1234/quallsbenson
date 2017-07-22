52d641da432286fc1514e6edf3c57472d3d63f3a
matt <jestir1234@gmail.com>
2017-07-22 00:01:51 -0400

    initialize proj

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="main-row1">
+          <div className="main-row1-box1">BOX1</div>
+          <div className="main-row1-box2">BOX2</div>
+        </div>
+
+        <div className="main-row2">
+          <div className="main-row2-box1">BOX1</div>
+          <div className="main-row2-box2">BOX2</div>
+        </div>
+
+        <div className="main-row3">
+          <div className="main-row3-box1">BOX1</div>
+          <div className="main-row3-box2">BOX2</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
