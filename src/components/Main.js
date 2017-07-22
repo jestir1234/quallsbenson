6293d06f0060e0af618370bd7c245662cb6a1256
matt <jestir1234@gmail.com>
2017-07-22 00:25:48 -0400

    add first box

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,13 @@ class Main extends Component {
     return (
       <div className="main-container">
         <div className="main-row1">
-          <div className="main-row1-box1">BOX1</div>
-          <div className="main-row1-box2">BOX2</div>
+          <div className="main-row1-box1">
+            <h1 className="main-box-header">The Journal</h1>
+            <p>Journal Squared invites a new generation to up and coming Jersey City Lorem ipsum dolor sit amet, auctor ut. Id est, bibendum conubia nunc non nibh, dui natoque mattis a, quam accumsan suspendisse tellus. Lectus amet nibh, a facilisis bibendum integer, est dapibus vitae adipiscing</p>
+          </div>
+          <div className="main-row1-box2">
+
+          </div>
         </div>
 
         <div className="main-row2">
